028d905714cf3a0193bd9f38a10895289b43b887
MinSeokKim <kimjunseok23@gmail.com>
2025-09-28 15:02:12 +0900

    Tailwindcss를 이요한 라이트/다크모드 추가 useContext를 이용한 전역관리리

--- Study/Week2/mission_2/src/App.tsx
+++ Study/Week2/mission_2/src/App.tsx
@@ -1,33 +1,38 @@
 import './App.css'
 import TodoInput from './components/TodoInput'
 import TodoList from './components/TodoList'
+import ThemeToggle from './components/ThemeToggle'
 import { TodoProvider } from './context/TodoContext'
+import { ThemeProvider } from './context/ThemeContext'
 
 function App() {
   return (
-    <TodoProvider>
-      <main className="todo-container">
-        <header className="todo-header">
-          <h1>MlNTY TODO</h1>
-        </header>
+    <ThemeProvider>
+      <TodoProvider>
+        <main className="todo-container">
+          <header className="todo-header">
+            <h1>MlNTY TODO</h1>
+            <ThemeToggle />
+          </header>
 
-        <TodoInput />
+          <TodoInput />
 
-        <section className="render-section">
-          <TodoList
-            title="할 일"
-            emptyMessage="할 일이 없습니다."
-            listType="pending"
-          />
+          <section className="render-section">
+            <TodoList
+              title="할 일"
+              emptyMessage="할 일이 없습니다."
+              listType="pending"
+            />
 
-          <TodoList
-            title="완료"
-            emptyMessage="완료된 일이 없습니다."
-            listType="completed"
-          />
-        </section>
-      </main>
-    </TodoProvider>
+            <TodoList
+              title="완료"
+              emptyMessage="완료된 일이 없습니다."
+              listType="completed"
+            />
+          </section>
+        </main>
+      </TodoProvider>
+    </ThemeProvider>
   )
 }
 
