071c2fa4add4ad133ac02dc790e6f8df9dc6e4a6
MinSeok1_2 <kimjunseok23@gmail.com>
2025-10-03 00:15:30 +0900

    mission_2

--- Study/Week3/mission_123/src/App.tsx
+++ Study/Week3/mission_123/src/App.tsx
@@ -1,12 +1,23 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
-import MoviePage from './pages/MoviePage'
+import Layout from './components/Layout'
+import HomePage from './pages/HomePage'
+import PopularPage from './pages/PopularPage'
+import NowPlayingPage from './pages/NowPlayingPage'
+import TopRatedPage from './pages/TopRatedPage'
+import UpcomingPage from './pages/UpcomingPage'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<MoviePage />} />
+        <Route element={<Layout />}>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/movies/popular" element={<PopularPage />} />
+          <Route path="/movies/now-playing" element={<NowPlayingPage />} />
+          <Route path="/movies/top-rated" element={<TopRatedPage />} />
+          <Route path="/movies/upcoming" element={<UpcomingPage />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   )
---
+++ Study/Week3/mission_123/src/pages/NowPlayingPage.tsx
@@ -0,0 +1,32 @@
+import { useState } from 'react'
+import MovieCard from '../components/MovieCard'
+import LoadingSpinner from '../components/LoadingSpinner'
+import Pagination from '../components/Pagination'
+import { useMovies } from '../hooks/useMovies'
+
+function NowPlayingPage() {
+  const [page, setPage] = useState(1)
+  const { movies, isLoading, isError, totalPages } = useMovies('now_playing', page)
+
+  if (isLoading) {
+    return <LoadingSpinner />
+  }
+
+  if (isError) {
+    return <div className="p-10 text-center text-red-500">에러가 발생했습니다.</div>
+  }
+
+  return (
+    <div className="p-10">
+      <h1 className="text-3xl font-bold text-gray-900 mb-6">상영 중인 영화</h1>
+      <div className="grid gap-4 grid-cols-2 sm:grid-cols-3 md:grid-cols-4 xl:grid-cols-6">
+        {movies.map(movie => (
+          <MovieCard key={movie.id} movie={movie} />
+        ))}
+      </div>
+      <Pagination currentPage={page} totalPages={totalPages} onPageChange={setPage} />
+    </div>
+  )
+}
+
+export default NowPlayingPage
