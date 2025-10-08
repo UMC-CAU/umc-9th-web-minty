07c7889d16f2737ea2d5209df997835c05c9a6a8
MinSeokKim <kimjunseok23@gmail.com>
2025-10-08 15:43:40 +0900

    Week4를 위한 directory reset

--- Study/Week4/src/App.tsx
+++ Study/Week4/src/App.tsx
@@ -1,21 +1,9 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import './App.css'
-import Layout from './components/Layout'
-import HomePage from './pages/HomePage'
-import MovieListPage from './pages/MovieListPage'
-import MovieDetailPage from './pages/MovieDetailPage'
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route element={<Layout />}>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/movies/:category" element={<MovieListPage />} />
-          <Route path="/movies/detail/:movieId" element={<MovieDetailPage />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <div>
+      <h1>Hello World</h1>
+    </div>
   )
 }
 
--- Study/Week4/src/components/LoadingSpinner.tsx
+++
@@ -1,9 +0,0 @@
-function LoadingSpinner() {
-  return (
-    <div className="flex justify-center items-center min-h-screen">
-      <div className="w-12 h-12 border-4 border-gray-200 border-t-rose-400 rounded-full animate-spin"></div>
-    </div>
-  )
-}
-
-export default LoadingSpinner
